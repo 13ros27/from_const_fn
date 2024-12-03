82918211625e58ff31e3166f8403ec86203d86d7
William Rose <william@theroses.me.uk>
2024-12-03 15:38:44 +0000

    Added a test to check the drop guard

--- tests/module_tests.rs
+++ tests/module_tests.rs
@@ -1,4 +1,6 @@
+use core::sync::atomic::{AtomicU32, Ordering};
 use from_const_fn::from_const_fn;
+use std::panic::catch_unwind;
 
 const fn multiply_by_2(n: usize) -> u8 {
     n as u8 * 2
@@ -21,4 +23,27 @@ fn check_correct_generation() {
     assert_eq!(correct, MULTIPLES_OF_2_BLOCK);
     assert_eq!(correct, MULTIPLES_OF_2_TYPE);
     assert_eq!(correct, MULTIPLES_OF_2_TYPES);
+}
+
+#[test]
+fn drop_check() {
+    static DROP_COUNTER: AtomicU32 = AtomicU32::new(0);
+    #[derive(Debug)]
+    struct Dropped;
+    impl Drop for Dropped {
+        fn drop(&mut self) {
+            DROP_COUNTER.fetch_add(1, Ordering::Relaxed);
+        }
+    }
+
+    catch_unwind(|| {
+        let _generated: [Dropped; 10] = from_const_fn!(|n| {
+            if n >= 5 {
+                panic!();
+            }
+            Dropped
+        });
+    })
+    .ok();
+    assert_eq!(DROP_COUNTER.load(Ordering::Relaxed), 5);
 }
