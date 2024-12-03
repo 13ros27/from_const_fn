046c3dd788c8ad97cf7003c3bb296dc63a421336
William Rose <william@theroses.me.uk>
2024-12-03 15:10:01 +0000

    Added a test for it

---
+++ tests/module_tests.rs
@@ -0,0 +1,8 @@
+#![allow(dead_code)]
+
+use from_const_fn::from_const_fn;
+
+const fn multiply_by_2(n: usize) -> u8 {
+    n as u8 * 2
+}
+const MULTIPLES_OF_2_FN: [u8; 50] = from_const_fn!(multiply_by_2, 50);
